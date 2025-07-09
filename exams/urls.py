4a2efb15180e3c233034715f3bd65418d82cd82e
wzw <zeroxyzwzw@gmail.com>
2025-07-09 11:24:26 +0800

    updates: update the tempaltes of exams and video

--- exams/urls.py
+++ exams/urls.py
@@ -5,7 +5,7 @@ app_name = 'exams'  # 定义命名空间
 
 urlpatterns = [
     path('', views.ExamListView.as_view(), name='exam_list'),
-    path('exams/<int:exam_id>/', views.StartExamView.as_view(), name='start_exam'),
-    path('exams/<int:exam_id>/submit/', views.SubmitExamView.as_view(), name='submit_exam'),
+    path('start/<int:exam_id>/', views.StartExamView.as_view(), name='start_exam'),
+    path('submit/<int:exam_id>/', views.SubmitExamView.as_view(), name='submit_exam'),
     path('result/<int:result_id>/', views.ResultView.as_view(), name='view_result'),
 ]
